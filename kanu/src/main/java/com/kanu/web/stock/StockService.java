7487277462c4f0f424ea645fce2c409458d3a310
min6340 <min6340@naver.com>
2018-06-20 16:23:22 +0900

    mybatis 공유용

--- kanu/src/main/java/com/kanu/web/stock/StockService.java
+++ kanu/src/main/java/com/kanu/web/stock/StockService.java
@@ -5,4 +5,11 @@ import java.util.Map;
 
 public interface StockService {
 	List<Map<String, Object>> getStockList();
+	StockVO getStock();
+	void insertStock(StockVO vo);
+	void updateStock(StockVO vo);
+	void deleteStock(String id);
+	void deleteStockList(List<String> list);
+	
+	
 }
